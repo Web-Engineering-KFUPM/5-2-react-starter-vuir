54769746ce3675b73e9434e317407b08d6e302e6
Vuir <Taevio@hotmail.com>
2025-10-01 12:03:50 +0300

    Made StudentCard Function

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,10 @@
+function StudentCard({ name, id, department}){
+    return(
+        <div>
+            <h3>Student's name: {name}</h3>
+            <p>Student's ID: {id}</p>
+            <p>Student's Department: {department}</p>
+        </div>
+        );
+}
+  export default StudentCard;
